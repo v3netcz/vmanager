1156f09a883665a54514fbfc593f0bae9d0c68d8
Adam Staněk <adam.stanek@v3net.cz>
2011-03-31 21:20:58 +0200

    Minimalni aplikace nad Nette
    Signed-off-by: Adam Staněk <adam.stanek@v3net.cz>

---
+++ app/bootstrap.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * My Application bootstrap file.
+ */
+
+
+use Nette\Debug;
+use Nette\Environment;
+use Nette\Application\Route;
+
+
+// Load Nette Framework
+// this allows load Nette Framework classes automatically so that
+// you don't have to litter your code with 'require' statements
+require LIBS_DIR . '/nette/Nette/loader.php';
+
+
+// Enable Nette\Debug for error visualisation & logging
+Debug::$strictMode = TRUE;
+Debug::enable(Debug::DEVELOPMENT);
+
+
+// Load configuration from config.neon file
+Environment::loadConfig();
+
+
+// Configure application
+$application = Environment::getApplication();
+$application->errorPresenter = 'Error';
+//$application->catchExceptions = TRUE;
+
+
+// Setup router
+$application->onStartup[] = function() use ($application) {
+	$router = $application->getRouter();
+
+	$router[] = new Route('index.php', 'Homepage:default', Route::ONE_WAY);
+
+	$router[] = new Route('<presenter>/<action>[/<id>]', 'Homepage:default');
+};
+
+
+// Run the application!
+$application->run();
