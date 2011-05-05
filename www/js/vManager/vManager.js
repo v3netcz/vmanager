6f08baa3a8dd23baf8ca81d3aea670aca095d2a9
Adam Staněk <adam.stanek@v3net.cz>
2011-05-05 18:16:51 +0200

    Lokalizace DatePickeru

--- www/js/vManager/vManager.js
+++ www/js/vManager/vManager.js
@@ -25,3 +25,30 @@ $(function () {
 			
 });
 		
+$(document).ready(function () {
+    $("input.date").each(function () { // input[type=date] does not work in IE
+        var el = $(this);
+        var value = el.val();
+        var date = (value ? $.datepicker.parseDate($.datepicker.W3C, value) : null);
+
+        var minDate = el.attr("min") || null;
+        if (minDate) minDate = $.datepicker.parseDate($.datepicker.W3C, minDate);
+        var maxDate = el.attr("max") || null;
+        if (maxDate) maxDate = $.datepicker.parseDate($.datepicker.W3C, maxDate);
+
+        // input.attr("type", "text") throws exception
+        if (el.attr("type") == "date") {
+            var tmp = $("<input/>");
+            $.each("class,disabled,id,maxlength,name,readonly,required,size,style,tabindex,title,value".split(","), function(i, attr)  {
+                tmp.attr(attr, el.attr(attr));
+            });
+            el.replaceWith(tmp);
+            el = tmp;
+        }
+        el.datepicker({
+            minDate: minDate,
+            maxDate: maxDate
+        });
+        el.val($.datepicker.formatDate(el.datepicker("option", "dateFormat"), date));
+    });
+});
